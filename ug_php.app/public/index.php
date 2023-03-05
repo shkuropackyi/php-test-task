c36ee1588c071493ae43cbd23d3e4f5289c4f34a
Shkuropackyi <shkuropackyi@gmail.com>
2023-03-05 17:15:29 +0200

    Create aproximately structure for a PHP project that uses only PHP and PDO to connect to a MySQL database

---
+++ ug_php.app/public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Project</title>
+</head>
+<body>
+    <h1>Welcome to My Project</h1>
+    <p>This is a general page for my project.</p>
+</body>
+</html>
